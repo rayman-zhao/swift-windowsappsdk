7b2ff446548f198b54c4d09c895087b71c10a432
rayman-zhao <zhao_yu@me.com>
2025-03-18 16:31:46 +0800

    Added generated files.

---
+++ Package.swift
@@ -0,0 +1,41 @@
+// swift-tools-version: 5.10
+
+import PackageDescription
+
+let linkerSettings: [LinkerSetting] = [
+/* Figure out magic incantation so we can delay load these dlls
+    .unsafeFlags(["-L\(currentDirectory)/Sources/CWinAppSDK/nuget/lib"]),
+    .unsafeFlags(["-Xlinker" , "/DELAYLOAD:Microsoft.WindowsAppRuntime.Bootstrap.dll"]),
+*/
+]
+
+let package = Package(
+    name: "swift-windowsappsdk",
+    products: [
+        .library(name: "WinAppSDK", type: .static, targets: ["WinAppSDK"]),
+        .library(name: "CWinAppSDK", targets: ["CWinAppSDK"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/rayman-zhao/swift-cwinrt", branch: "main"),
+        .package(url: "https://github.com/rayman-zhao/swift-windowsfoundation", branch: "main"),
+        .package(url: "https://github.com/rayman-zhao/swift-uwp", branch: "main"),
+    ],
+    targets: [
+        .target(
+            name: "WinAppSDK",
+            dependencies: [
+                .product(name: "CWinRT", package: "swift-cwinrt"),
+                .product(name: "WindowsFoundation", package: "swift-windowsfoundation"),
+                .product(name: "UWP", package: "swift-uwp"),
+                "CWinAppSDK"
+            ]
+        ),
+        .target(
+            name: "CWinAppSDK",
+            resources: [
+                .copy("nuget/bin/Microsoft.WindowsAppRuntime.Bootstrap.dll"),
+            ],
+            linkerSettings: linkerSettings
+        ),
+    ]
+)
