efcf3d50bdc94f1f17d27abc1b042ebf6f9ed9c7
rayman-zhao <zhao_yu@me.com>
2026-02-25 14:21:47 +0800

    Fixed wrong dependencies

--- Package.swift
+++ Package.swift
@@ -16,12 +16,9 @@ let package = Package(
         .library(name: "CWinAppSDK", targets: ["CWinAppSDK"]),
     ],
     dependencies: [
-        // .package(url: "https://github.com/rayman-zhao/swift-cwinrt", branch: "main"),
-        // .package(url: "https://github.com/rayman-zhao/swift-windowsfoundation", branch: "main"),
-        // .package(url: "https://github.com/rayman-zhao/swift-uwp", branch: "main"),
-        .package(path: "../swift-cwinrt"),
-        .package(path: "../swift-windowsfoundation"),
-        .package(path: "../swift-uwp"),
+        .package(url: "https://github.com/rayman-zhao/swift-cwinrt", branch: "main"),
+        .package(url: "https://github.com/rayman-zhao/swift-windowsfoundation", branch: "main"),
+        .package(url: "https://github.com/rayman-zhao/swift-uwp", branch: "main"),
     ],
     targets: [
         .target(
